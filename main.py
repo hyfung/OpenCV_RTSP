e197d39aee31f4bf6cc2fee04807d087ee6cc3c2
Jerry Fung <hyfung@link.cuhk.edu.hk>
2020-12-05 14:09:52 +0800

    WIP

--- main.py
+++ main.py
@@ -16,10 +16,30 @@ streams = {
     2: rtsp_360p,
 }
 
+img = None
+
+def record_on_motion():
+    """
+    Sibling thread to record for 30 seconds on motion detected
+    """
+    global is_recording
+    is_recording = True
+    filename = ".avi"
+    cv2.VideoWriter()
+    while elapsed < 30:
+
+
+    pass
+
 def main():
+    should_record = False
+
     ap = argparse.ArgumentParser()
     ap.add_argument("-s", "--src", help="Resolution: 1=1080p, 2=360p", type=int, default=2)
     ap.add_argument("-f", "--file", help="file name to save to", type=str)
+    ap.add_argument("-m", "--motion", help="Enable motion detection", action="store_true")
+    ap.add_argument("-r", "--record-on-motion", help="Record an AVI on motion detected", action="store_true")
+    ap.add_argument("-l", "--headless", help="Do not show frame", action="store_true")
     args = vars(ap.parse_args())
 
     cap = cv2.VideoCapture(streams[args['src']])
@@ -29,10 +49,23 @@ def main():
         pass
     
     while True:
+        global img
         ret, img = cap.read()
-        cv2.imshow('mat', img)
         
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if args['motion']:
+            # Motion Detection Algorithm, based on previous 5 frames
+
+            # Calculate "Motion Score" to filter noise
+
+            if should_record:
+                record_thread = threading.Thread(record_on_motion)
+                record_thread.start()
+            pass
+
+        if not args["headless"]:
+            cv2.imshow('mat', img)
+
+        if cv2.waitKey(33) & 0xFF == ord('q'):
             break    
         
 
