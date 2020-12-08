67d7493041155732c5255df679258870c7bc973f
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-08 10:17:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 #include "opencv2/opencv.hpp"
+
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+    cv::Mat frame;
+
+    cv::VideoCapture cap;
+    cap.open(0);
+
+    if(!cap.isOpened())
+    {
+        std::cout << "Failed opening capture device" << std::endl;
+        return -1;
+    }
+
+    while(1)
+    {
+
+        cap.read(frame);
+
+        cv::imshow("Frame", frame);
+
+        if(cv::waitKey(33) >=  0)
+        {
+            break;
+        }
+
+    }
+
+    return 0;
+}
